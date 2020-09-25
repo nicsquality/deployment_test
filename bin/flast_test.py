3c81c8ec97999f7afc954d827120c79844902dab
Nikhil Pawar <nikhil.pawar03@gmail.com>
2020-09-26 00:53:43 +0530

    add flask test

---
+++ bin/flast_test.py
@@ -0,0 +1,10 @@
+import flask
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/add', methods=['GET'])
+def addition():
+    return 'Sum of 2 and 3 is: {}'.format(2 + 3)
+
+app.run(host='localhost', port=8023)
